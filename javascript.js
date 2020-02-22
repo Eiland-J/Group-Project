e9b633227109ce748cf397c8dffe1499c30ae3fb
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-02-22 13:34:03 -0700

    updated variables and added randomLetterGen function

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ var losses = 0;
 
 //Holds user inputs in an array 
 var yourChoices = [];
+var letterToGuess = null;
+var totalGuesses = 10;
+
+function randomLetterGen() {
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log(letterToGuess);
+}
+randomLetterGen();
+function updatedTotalGuesses(){
+    document.querySelector("#guesses-left").innerHTML = totalGuesses;
+}
+
